68bc7bb02dec5e609f334f83adeb40aed619fad8
liviuxyz-ctrl <lviu2002liviu@gmail.com>
2024-06-06 13:38:51 +0300

    Adding the workspace.xml file

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-# src/main.py
 import os
+import argparse
 from src.data.database import DatabaseManager
 from src.ingestion.load import store_financial_data, store_commodity_data
 from src.data.models import initialize_cassandra_connection, create_financial_data_model, create_commodity_data_model
 from init_scripts.populate_sp500_data import get_or_create_source_id, get_or_create_asset_id, populate_sp500_data
-from init_scripts.populate_commodities_data import populate_commodities_data
 
 
-def main():
+def main(limit_sp500):
     # Set environment variable for schema management
     os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
 
@@ -19,11 +18,8 @@ def main():
         # Initialize Cassandra connection
         initialize_cassandra_connection()
 
-        # Populate SP500 data without limit (full run)
-        populate_sp500_data(limit=1)
-
-        # Populate Commodities data
-        # populate_commodities_data()
+        # Populate SP500 data with specified limit
+        populate_sp500_data(limit=limit_sp500)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -32,4 +28,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Load SP500 financial data into the database.")
+    parser.add_argument('--sp500', type=int, default=500, help='Number of S&P 500 entries to populate.')
+    args = parser.parse_args()
+
+    main(limit_sp500=args.sp500)
